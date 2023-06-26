7c876bd0160bc007fa74614f27c9cdec8b14a9dc
Tycho Cinquini <tcinquini@intern3.localdomain>
2023-06-26 16:14:46 -0600

    Modified parse.py - Added comment headers to functions - Added function for parsing .dat files

--- flatirons/parse.py
+++ flatirons/parse.py
@@ -1,5 +1,12 @@
 import numpy as np
+import struct   
 
+# csv_parse() extracts the IQ data from a .csv data file
+#
+# Inputs:
+#   csv_file : filepath for data file                                                [.csv]
+#
+# Outputs: IQ data for up to two signals (I1,Q1) and (I2,Q2)     [numpy arrays of type int]
 def csv_parse(csv_file):
     """
     Parses a sample csv file into IQ arrays
@@ -30,3 +37,16 @@ def csv_parse(csv_file):
             return np.array(I1), np.array(Q1)
         else:
             return np.array(I1),np.array(Q1),np.array(I2),np.array(Q2)
+
+# dat_parse() extracts the IQ data from a .dat file
+#
+# Inputs:
+#   dat_file : filepath for data file
+#
+# Outputs: IQ data for signal (I,Q)                              [numpy arrays of type int]
+def dat_parse(dat_file, num_samples):
+    with open(dat_file, mode="rb") as fs:
+        data = fs.read()
+        I = [int.from_bytes(data[i:i+2], byteorder='little', signed=True) for i in range(0, len(data), 4)]
+        Q = [int.from_bytes(data[i+2:i+4], byteorder='little', signed=True) for i in range(0, len(data), 4)]
+        return np.array(I), np.array(Q)
