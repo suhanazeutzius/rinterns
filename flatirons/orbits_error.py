a6b668b253cc697bc7ecabf44a707364fbd6ce54
Tycho Cinquini <tcinquini@intern3.localdomain>
2023-07-11 18:03:45 -0600

    Began work on orbit propogation and error analysis [flatirons/orbits_error.py] - Created new file to store functions used for orbit propogation and error analysis - Created new function get_overhead_satellites to display the satellites currently overhead of the receier
    [orbits_test.py]
    - Created script for testing orbit propogation and error analysis

---
+++ flatirons/orbits_error.py
@@ -0,0 +1,57 @@
+# Import required python packages
+from skyfield.api import load, wgs84, EarthSatellite
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load mplstyle file
+plt.style.use('flatirons/flatirons.mplstyle')
+
+def get_overhead_satellites(t, tle_file, field_of_view, receiver_latlon, debug=False):
+    # Load satellite ephemeris data from TLE file
+    satellites = load.tle_file(tle_file)
+
+    # Create dictionary of GPS PRNs
+    prn_dict = {int(sat.name[-3:-1]): sat for sat in satellites}
+
+    if debug:
+        # Display which satellites were loaded
+        print("Loaded", len(satellites), "satellites:")
+        for prn in prn_dict.keys():
+            print("PRN " + str(prn))
+
+    # Create object to store receiver position
+    receiver = wgs84.latlon(receiver_latlon[0], receiver_latlon[1])
+
+    # Iterate over all satellites
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    for prn in range(33):
+        if prn in prn_dict.keys():
+            # Extract satellite object corresponding to current prn
+            sat = prn_dict[prn]
+            
+            # Calculate distance between satellite and reciever
+            sat_range = sat - receiver
+
+            # Calculate elevation, azimuth, and distance to satellite
+            alt, az, distance = sat_range.at(t).altaz()
+            el = 90-alt.degrees
+
+            # Plot satellite
+            if el <= field_of_view:
+                ax.scatter(az.degrees, el, s=50, color='#DBB40C', zorder=2)
+                ax.annotate(prn, (az.degrees, el+7), color='#DBB40C', fontweight='bold', zorder=2)
+            else:
+                ax.scatter(az.degrees, el, s=50, color='#C0C0C0')
+
+    # Plot FOV
+    theta = np.linspace(0, 360, num=1000)
+    r = field_of_view*np.ones(1000)
+    ax.plot(theta, r, zorder=1)
+
+    # Make plot look nice
+    ax.set_yticklabels([])
+    ax.set_xticklabels(['N','45°','E','135°','S','225°','W','315°'])
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+    ax.set_rmax(90)
+    plt.show()
--- orbit_test.py
+++ orbit_test.py
@@ -1,36 +1,19 @@
 # Import required python packages
-from skyfield.api import load, wgs84
+from skyfield.api import load, wgs84, EarthSatellite
 import numpy as np
 import matplotlib.pyplot as plt
 
-stations_url = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle'
-satellites = load.tle_file(stations_url)
-gps_prns = {int(sat.name[-3:-1]): sat for sat in satellites}
-print('Loaded', len(satellites), 'satellites:')
-for prn in gps_prns.keys():
-    print("PRN " + str(prn))
+# Import custom python packages
+from flatirons.orbits_error import *
 
-receiver = wgs84.latlon(+39.58709, -104.82873)
-    
+# Load mplystyle file
+plt.style.use('flatirons/flatirons.mplstyle')
+
+#stations_url = 'http://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle'
+#satellites = load.tle_file(url=stations_url, filename='tle-gps.txt', reload=True)
+gps_file = 'tle-gps.txt'
 ts = load.timescale()
-t = ts.utc(2023, 7, 10, 20, 5, 30)
+t = ts.utc(2023, 7, 6, 10, 20, 0)
+#t = ts.now()
 
-fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-for prn in range(33):
-    if prn in gps_prns.keys():
-        sat = gps_prns[prn]
-        sat_range = sat - receiver
-    
-        alt, az, distance = sat_range.at(t).altaz()
-        el = 90-alt.degrees
-        if el  <= 26.2:
-            ax.scatter(az.degrees, el)
-        else:
-            ax.scatter(az.degrees, el, color='#808080')
-
-theta = np.linspace(0,360,num=1000)
-r = 26.2*np.ones(1000)
-
-ax.plot(theta,r)
-ax.set_rmax(90)
-plt.show()
+get_overhead_satellites(t, gps_file, 26.2, [+39.58709, -104.82873], debug=True)
